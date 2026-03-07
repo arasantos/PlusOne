17d0c655fa404ae21fafc93e86aa9f6465c49b45
Ivy Ding <187065443+Ivy-Ding@users.noreply.github.com>
2026-03-07 02:42:53 -0600

    Add preferences.js data

---
+++ assets/js/preferences.js
@@ -0,0 +1,22 @@
+export const PREFERENCES = {
+    TRANSIT_ONLY: {
+        id: "transit_only",
+        label: "Transit Only",
+        icon: "fa-solid fa-bus",
+    },
+    PARTIAL: {
+        id: "partial",
+        label: "Partial",
+        icon: "fa-solid fa-handshake",
+    },
+    FULL: {
+        id: "full",
+        label: "Full Event",
+        icon: "fa-solid fa-children",
+    },
+    FLEXIBLE: {
+        id: "flexible",
+        label: "Flexible",
+        icon: "fa-solid fa-rotate",
+    },
+}
